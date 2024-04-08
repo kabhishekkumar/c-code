8051f66a281a8b56d1600b321885194b399e4e13
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-08 21:40:08 +0530

    print the reverse array

--- array file/reverse_array.cpp
+++ array file/reverse_array.cpp
@@ -9,7 +9,7 @@ cout<<"enter the value of array";
     for(i=0; i<n; i++){
 cin>>a[i];
     }
-
+  cout<<"reverse array"<<endl;
     for(i=(n-1); i>=0; i--){
         cout<<a[i];
         cout<<"\n";
@@ -18,4 +18,5 @@ cin>>a[i];
     //     cout<<a[i];
     //     cout<<"\n";
     // }
+    return 0;
 }
