9213d98645327e37e6f6b146cf82122bb304e9f3
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-13 23:29:36 +0530

    copy the array element in another array

---
+++ PATTERN/squrePattern.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter the value of n:";
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        for(j=1; i<=n; j++){
+            if(j==1 && j<=n){
+                cout<<"*";
+            }
+           
+        }
+       if(i==5 && j<=n ){
+        cout<<"*";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
