53f92a8cd1f0b9c8bdbac31398c2bcca8890452a
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-04-24 23:54:44 +0530

    print array element

--- PATTERN/squrePattern.cpp
+++ PATTERN/squrePattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int i,j,n;
-    cout<<"enter the value of n:";
+    cout<<"enter the value of the n:";
     cin>>n;
 
     for(i=1; i<=n; i++){
@@ -17,5 +17,5 @@ int main(){
        }
        cout<<endl;
     }
-   
+
 }
