7b5aee7943b4bbac3cb154f7bed554f5c48038b3
kabhishekkumar <kabhishekkumar00266@gmail.com>
2024-05-20 22:11:39 +0530

    find the maximum value of array

--- string in c++/multiple.cpp
+++ string in c++/multiple.cpp
@@ -1,19 +1,25 @@
 #include<iostream>
-
 using namespace std;
+
+class A{
+  public:
+
+ virtual void fun(){
+    cout<<"abhishek"<<endl;
+  }
+};
+
+class B : public A{
+  public:
+
+  void fun(){
+    cout<<"govinda "<<endl;
+  }
+};
+
 int main(){
-    int i;
-    char a[5][20];
-    cout<<"enter the value of four string";
-   for(i=0; i<=3; i++){
-    gets(a[i]);
-   }
-   for(i=0; i<=3; i++){
-    // puts(a[i]);
-  cout<<puts(a[i])<<endl;
- 
-   }
-   
-  
-   
+  A *ptr; B obj;
+
+ ptr=&obj;
+ ptr->fun();
 }
